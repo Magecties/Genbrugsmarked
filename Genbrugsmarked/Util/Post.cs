eed2a9fb1c9cbdcb1a7cbfc6b04161ad4b0f9232
Benwi01 <Benjamin060101@gmail.com>
2024-04-24 13:58:51 +0200

    forsøg med at få min profil side til at køre (virker ikke)

--- Genbrugsmarked/Util/Post.cs
+++ Genbrugsmarked/Util/Post.cs
@@ -1,21 +1,19 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson;
 
-
-namespace Core
+namespace Genbrugsmarked.Util
 {
     public class Post
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public ObjectId Id { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
         public string Room { get; set; }
 
-        public string Description { get; set; }
-
         public string status { get; set; }
+
+        public string Description { get; set; }
 
         public bool IsActive { get; set; }
     }
